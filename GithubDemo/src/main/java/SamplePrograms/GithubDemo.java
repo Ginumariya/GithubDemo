a6d3f7f323df471d29d09f46a62c8ae498d16400
Ginumariya <ginumariyaraju1111@gmail.com>
2023-04-24 10:01:36 +0530

    updated git

--- GithubDemo/src/main/java/SamplePrograms/GithubDemo.java
+++ GithubDemo/src/main/java/SamplePrograms/GithubDemo.java
@@ -3,6 +3,7 @@ package SamplePrograms;
 public class GithubDemo {
 	public static void main (String args[]) {
 		System.out.println("for commiting to Github");
+		System.out.println("changes for github");
 		
 	}
 
